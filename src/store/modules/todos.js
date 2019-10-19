e20a528380c246b022219acb18e00b7885cb5b3e
erka¨git config --global user.name erkagit config --global user.name erka¨ <erik.westerberg@hotmail.se>
2019-10-19 16:19:02 +0200

    creates a action inside of module that fetches todos from a api using axios,

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,23 +1,24 @@
+import Axios from "axios";
+
 // import axios from "axios";
 
 const state = {
-  todos: [
-    {
-      id: 1,
-      title:"Todo One"
-    },
-    {
-      id:2,
-      title:"Todo Two"
-    }
-  ]
+  todos: []
 };
 
 const getters = {
   allTodos: (state) => state.todos
 };
 
-const actions = {};
+//commit is used for calling the mutation
+const actions = {
+  async fetchTodos({ commit }) {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/todos'
+    );
+    console.log(response.data)
+  }
+};
 
 const mutations = {};
 
