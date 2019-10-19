01f6d6a7ec2f23217246ccd5b441452292e0831f
erka¨git config --global user.name erkagit config --global user.name erka¨ <erik.westerberg@hotmail.se>
2019-10-19 16:39:44 +0200

    creates a new action with a post request to add a new todo, trough commit, call the mutation thats holds the function where i pass the response

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -5,21 +5,29 @@ const state = {
 };
 
 const getters = {
-  allTodos: (state) => state.todos
+  allTodos: state => state.todos
 };
 
 //commit is used for calling the mutation
 const actions = {
   async fetchTodos({ commit }) {
     const response = await axios.get(
-      'https://jsonplaceholder.typicode.com/todos'
+      "https://jsonplaceholder.typicode.com/todos"
     );
-    commit('setTodos', response.data);
+    commit("setTodos", response.data);
+  },
+  async addTodo({ commit }, title) {
+    const response = await axios.post(
+      "https://jsonplaceholder.typicode.com/todos",
+      { title, completed: false }
+    );
+    commit("newTodo", response.data);
   }
 };
 
 const mutations = {
-  setTodos: (state, todos) => (state.todos = todos)
+  setTodos: (state, todos) => (state.todos = todos),
+  newTodo: (state, todo) => state.todos.unshift(todo)
 };
 
 export default {
@@ -27,4 +35,4 @@ export default {
   getters,
   actions,
   mutations
-}
+};
